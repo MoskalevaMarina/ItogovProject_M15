a60ac63fc0486b34d1f57513d625ac3bc81a3144
MoskalevaMarina <mari10102008@yandex.ru>
2021-10-12 17:03:03 +0400

    Добавьте файлы проекта.

---
+++ ItogovProject_M15/Program.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="Program.cs" company="My Company Marina">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ItogovProject_M15
+{
+    /// <summary>
+    /// Класс Program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Метод Main
+        /// </summary>
+        public static void Main()
+        {
+          var classes = new[]
+          {
+               new Classroom
+               {
+                   Students =
+                   {
+                       "Evgeniy", "Sergey", "Andrew"
+                   },
+               },
+               new Classroom
+               {
+                   Students =
+                   {
+                       "Anna", "Viktor", "Vladimir"
+                   },
+               },
+               new Classroom
+               {
+                   Students =
+                   {
+                   "Bulat", "Alex", "Galina"
+               },
+            }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод получения списка всех студентов
+        /// </summary>
+        /// <param name="classes">Ввод классных комнат</param>
+        /// <returns> результат сформированный список студентов </returns>
+      public static string[] GetAllStudents(Classroom[] classes)
+        {
+            var rezult = (from room in classes
+                          from item in room.Students
+                          select item.ToString()).ToArray();
+
+            return rezult;
+        }
+
+        /// <summary>
+        /// Класс Classroom
+        /// </summary>
+        public class Classroom
+        {
+            /// <summary>
+            /// Поле для хранения списка студентов
+            /// </summary>
+            public List<string> Students = new List<string>();
+        }
+    }
+}
